892812ad6ca6094c2b247e4de1c7c2b1708152e8
dimuthi tharaka <dimuthi@gmail.com>
2022-01-06 17:33:12 +0530

    UI created

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+
+const AddUser = (props) => {
+  const [EnteredUsername, setEnteredUsername] = useState("");
+  const [Enteredage, setEnteredage] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (EnteredUsername.trim().length === 0 || Enteredage.trim().length === 0) {
+      return;
+    }
+    if (+Enteredage < 1) {
+      return;
+    }
+    console.log(Enteredage,EnteredUsername);
+    setEnteredUsername("");
+    setEnteredage("");
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredage(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="userName">Add User</label>
+        <input
+          type="text"
+          id="userName"
+          onChange={userNameChangeHandler}
+          value={EnteredUsername}
+        ></input>
+        <label htmlFor="age">Age (Years)</label>
+        <
